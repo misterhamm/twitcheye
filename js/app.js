103c5d4ec863982e70f363783c4c64b04b02acec
Chris Hamm <chris.m.hamm@gmail.com>
2015-07-06 15:20:37 -0400

    second html file linked

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ $(document).ready(function() {
         console.log(searchTerms);
         getRequest(searchTerms);
     });
+
+    $.getJSON('http://www.giantbomb.com/api/game/30475/?api_key=62aa06d41a05735519c6863554fdb36fbbb347e4&format=jsonp&json_callback=?', function(data) {
+        console.log(data.name);
+    });
 });
 
 
@@ -27,15 +31,22 @@ function getRequest(searchTerms) {
 function showResults(results) {
     var displayName = "";
     $.each(results, function(index, value) {
-        displayName = '<p>' + value.name + '</p>';
+        displayName = '<h1><a href="detail.html">' + value.name + '</a></h1>';
         displayDeck = '<p>' + value.deck + '</p>';
-        displayThumb = '<p><img src="' + value.image.thumb_url + '"></p>';
+        displayThumb = '<p><a href="detail.html"><img src="' + value.image.thumb_url + '"></a></p>';
         console.log(value.deck);
         $('.results').append(displayName + displayDeck + displayThumb);
     });
 }
 
-
+/*
+var headerFill = function(index, value) {
+    displayArt = '<img src="' + value.image.super_url + '">';
+    displayTitle = '<h1>' + value.name + '</h1>';
+    displayDeck = '<p>' + value.deck + '</p>';
+    $('.detail-header').html(displayArt + + displayTitle + displayDeck);
+}
+*/
 
 
 
